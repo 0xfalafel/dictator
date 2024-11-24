4e22782da5278ad74bb019afaa8a9436616bdcea
Olivier Lasne <lasneolivier@gmail.com>
2024-11-25 00:09:56 +0100

    started working on a tokio::spawn() example

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
 use global_hotkey::{GlobalHotKeyEvent, GlobalHotKeyManager, HotKeyState, hotkey::{HotKey, Modifiers, Code}};
 use std::thread;
-use std::time::Duration;
 
-fn main() {
+use tokio::sync::mpsc;
+use tokio::time::{sleep, Duration};
+use tokio::task::spawn;
+use tokio::select;
+
+
+async fn demo(mut rx: mpsc::Receiver<()>) {
+    let mut count: u64 = 0;
+    
+    loop {
+        if rx.recv().await.is_some() {
+            return;
+        }
+
+        println!("{}", count);
+        count += 1;
+    }
+}
+
+
+#[tokio::main]
+async fn main() {
     // initialize the hotkeys manager
     let manager = GlobalHotKeyManager::new().unwrap();
     let hotkey = HotKey::new(Some(Modifiers::SUPER), Code::KeyM);
@@ -12,13 +32,18 @@ fn main() {
     });
 
 
+    // channels used to stop the counter
+    let (tx, rx) = mpsc::channel(1);
+
     loop {
         
         if let Ok(event) = GlobalHotKeyEvent::receiver().try_recv() {
             // println!("{:?}", event.state);
 
             match event.state {
-                HotKeyState::Pressed  => println!("Hi mom!"),
+                HotKeyState::Pressed  => {
+                    tokio::spawn(demo(rx));
+                },
                 HotKeyState::Released => println!("Bye mom!"),
             }
         }
