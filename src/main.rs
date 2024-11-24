bba1d8231efb382bfe9ac4fb0ef19f75fc4fa941
Olivier Lasne <lasneolivier@gmail.com>
2024-11-24 22:00:42 +0100

    register a global shortcut

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use global_hotkey::{GlobalHotKeyEvent,GlobalHotKeyManager, hotkey::{HotKey, Modifiers, Code}};
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hi mom!");
+    // initialize the hotkeys manager
+    let manager = GlobalHotKeyManager::new().unwrap();
+    let hotkey = HotKey::new(Some(Modifiers::SUPER), Code::KeyM);
+    manager.register(hotkey).err().map(|e| {
+        eprintln!("Could not register the global hotkey Super+M: {}", e);
+        std::process::exit(1);
+    });
+
+
+    loop {
+        
+        if let Ok(event) = GlobalHotKeyEvent::receiver().try_recv() {
+            println!("{:?}", event);
+        }
+
+        thread::sleep(Duration::from_millis(100));
+    }
 }
